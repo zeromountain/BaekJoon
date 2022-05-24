02bdd8960b322648943bba45a8a1acbe8c856f1a
손영산 <54147313+zeromountain@users.noreply.github.com>
2022-05-25 08:53:01 +0900

    [Silver III] Title: N과 M (6), Time: 112 ms, Memory: 9616 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+function sol(input) {
+  const [N, M] = input[0].split(' ').map(Number);
+  const sequence = input[1].split(' ').map(Number);
+  sequence.sort((a, b) => a - b);
+
+  const result = [];
+  const temp = [];
+  const check = Array.from({ length: N }, () => 0);
+
+  function dfs(l, start) {
+    if (l === M) {
+      result.push(temp.slice());
+      return;
+    } else {
+      for (let i = start; i < N; i++) {
+        if (!check[i]) {
+          check[i] = 1;
+          temp.push(sequence[i]);
+          dfs(l + 1, i + 1);
+          temp.pop();
+          check[i] = 0;
+        }
+      }
+    }
+  }
+
+  dfs(0, 0);
+  // console.log(result);
+  return result
+    .map(el => el.join(' '))
+    .reduce((acc, curr) => {
+      acc += curr + '\n';
+      return acc;
+    }, '')
+    .trim();
+}
+
+console.log(sol(input));
